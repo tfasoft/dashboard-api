c38fe72402074c92c59501e285b2c27dcdf7949b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-16 21:28:54 +0430

    Clean

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require('express');
-const session = require('express-session');
 const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const randomstring = require('randomstring');
 
 const Admin = require('./modules/admin');
-const User = require('./modules/user');
 
-require('dotenv').config();
 const env = process.env;
-
-const oneDay = 1000 * 60 * 60 * 24;
 
 const app = express();
 
-app.set('trust proxy', 1);
-app.set('view engine', 'ejs');
-
-app.use(session({
-    secret: 'thesecretkey',
-    resave: false,
-    cookie: { maxAge: oneDay },
-    saveUninitialized: true
-}))
-
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.set('json spaces', 2);
 
 const mdb = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@${env.MONGO_DATABASE}.ji4jf.mongodb.net/${env.MONGO_COLLECTION}?retryWrites=true&w=majority`;
 mongoose.connect(mdb)
@@ -36,59 +23,13 @@ mongoose.connect(mdb)
     .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+    res.header("Access-Control-Allow-Origin", "*");
 
-app.get('/panel', (req, res) => {
-    if (!req.session.user) res.redirect('/auth');
-    else {
-        Admin.findById(req.session.user)
-            .then((user) => {
-                res.render('panel', {user});
-            })
-            .catch((error) => res.send(error));
+    res.status(200);
+
+    const object = {
+        'message': 'welcome to TFA dashboard API service'
     }
-});
 
-app.get('/auth', (req, res) => {
-    if (req.session.user) res.redirect('/panel');
-    else res.render('auth');
-});
-
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-});
-
-app.post('/login', (req, res) => {
-    Admin.findOne(req.body)
-        .then((user) => {
-            if (user === null) {
-                res.send('User not found.')
-            } else {
-                req.session.user = user.id;
-
-                res.redirect('/panel');
-            }
-        })
-        .catch((error) => res.send(error));
-});
-
-app.post('/register', (req, res) => {
-    const data = {
-        access_token: randomstring.generate({length: 25, charset: 'alphabetic'}),
-        name: req.body.name,
-        username: req.body.username,
-        password: req.body.password,
-    };
-
-    const newAdmin = new Admin(data);
-
-    newAdmin.save()
-        .then((result) => {
-            req.session.user = result.id;
-
-            res.redirect('/panel');
-        })
-        .catch((error) => res.send(error));
+    res.send(object);
 });
