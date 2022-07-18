9b208eaf5272ea269159e9578a7934f7196b0b24
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 15:32:17 +0430

    Add change name

--- app/controllers/ChangeControllers.js
+++ app/controllers/ChangeControllers.js
@@ -4,36 +4,23 @@ const Admin = require("../models/admin");
 const changeName = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    // Admin.findOne(req.body)
-    //     .then((user) => {
-    //         if (user === null) {
-    //             const outData = {
-    //                 error: "user not found"
-    //             }
-    //
-    //             res.status(401);
-    //             res.send(outData);
-    //         } else {
-    //             const outData = {
-    //                 id: user._id
-    //             }
-    //
-    //             res.status(200);
-    //             res.send(outData);
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         const outData = {
-    //             error: error.message
-    //         }
-    //
-    //         res.status(500);
-    //         res.send(outData);
-    //     });
+    Admin.findByIdAndUpdate(req.body.id, {name: req.body.name})
+        .then((result) => {
+            const outData = {
+                "message": "name changed"
+            }
 
-    res.status(200);
+            res.status(200);
+            res.send(outData);
+        })
+        .catch((error) => {
+            const outData = {
+                error: error.message
+            }
 
-    res.send(req.body);
+            res.status(500);
+            res.send(outData);
+        });
 }
 
 module.exports = {
