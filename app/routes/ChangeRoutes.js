fbaf90cae141e3925290276a269cf6998075e185
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 19:18:39 +0430

    Add delete route

--- app/routes/ChangeRoutes.js
+++ app/routes/ChangeRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/name', controllers.changeName); // Route change name
 router.post('/password', controllers.changePassword); // Route change password
 router.post('/username', controllers.changeUsername); // Route change username
+router.post('/delete', controllers.deleteUser); // Route delete user
 
 module.exports = router;
