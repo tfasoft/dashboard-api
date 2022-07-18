06015a8f178e74faca32d481bb9d233bc34bca28
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 14:22:54 +0430

    Create routes for none

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,8 +5,5 @@ const controllers = require('../controllers/controllers');
 const router = express.Router();
 
 router.get('/', controllers.index); // Route index
-router.post('/login', controllers.loginAdmin); // Route login admin
-router.post('/register', controllers.registerAdmin); // Route register admin
-router.post('/get/admin', controllers.getAdmin); // Route get admin
 
 module.exports = router;
