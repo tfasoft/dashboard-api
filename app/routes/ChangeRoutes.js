9bc10431b734405578b36c7f0b7443812f8ab574
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 16:59:58 +0430

    Add password and username routes

--- app/routes/ChangeRoutes.js
+++ app/routes/ChangeRoutes.js
@@ -4,6 +4,8 @@ const controllers = require('../controllers/ChangeControllers');
 
 const router = express.Router();
 
-router.post('/name', controllers.changeName); // Route login admin
+router.post('/name', controllers.changeName); // Route change name
+router.post('/password', controllers.changePassword); // Route change password
+router.post('/username', controllers.changeUsername); // Route change username
 
 module.exports = router;
