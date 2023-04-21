4c8f38dfdc860d8e7575fdb97e005985ccb7bc86
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-21 12:32:37 +0330

    Add config for mail

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,7 @@
 import appConfig from "$app/config/app/app.config.js";
+import mailConfig from "$app/config/mail/mail.config.js";
 import fileConfig from "$app/config/file/file.config.js";
 import zarinpalConfig from "$app/config/zarinpal/zarinpal.config.js";
 import databaseConfig from "$app/config/database/database.config.js";
 
-export { appConfig, fileConfig, databaseConfig, zarinpalConfig };
+export { appConfig, fileConfig, databaseConfig, zarinpalConfig, mailConfig };
---
+++ app/config/mail/mail.config.js
@@ -0,0 +1,6 @@
+import env from "$app/env/index.js";
+
+export default {
+  address: env.MAIL_ADDRESS,
+  password: env.MAIL_PASSWORD,
+};
