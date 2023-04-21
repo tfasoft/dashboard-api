07db092cd8d3ad541e59b77454adf79ce38eb9b8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-21 04:41:38 +0330

    Add zarinpal controllers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,5 @@
+import * as Payment from "$app/controllers/payment/payment.controllers.js";
 import * as Auth from "$app/controllers/auth/auth.controllers.js";
 import * as User from "$app/controllers/user/user.controllers.js";
 
-export { Auth, User };
+export { Payment, Auth, User };
---
+++ app/controllers/payment/payment.controllers.js
@@ -0,0 +1,84 @@
+import { User, Payment } from "$app/models/index.js";
+
+import { zarinpal } from "$app/functions/index.js";
+
+export const REQUEST = async (req, res) => {
+  const { credits, user } = req.body;
+
+  try {
+    const userData = await User.findById(user);
+
+    zarinpal
+      .PaymentRequest({
+        Amount: credits,
+        CallbackURL: "https://dashboard.tfasoft.com/api/payment/verify",
+        Description: "Buy credits for authentication",
+        Email: userData.email,
+        Mobile: userData.phoneNumber,
+      })
+      .then(async (response) => {
+        if (response.status === 100) {
+          try {
+            await Payment.create({
+              authority: response.authority,
+              user,
+              amount: credits,
+            });
+
+            res.status(200).send({ url: response.url });
+          } catch (error) {
+            res.status(500).send({ message: error.message });
+          }
+        }
+      })
+      .catch((error) => {
+        res.status(500).send({ message: error.message });
+      });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+export const VERIFY = async (req, res) => {
+  const data = req.query;
+
+  if (data.Status === "OK") {
+    try {
+      const payment = await Payment.findOne({ authority: data.Authority });
+
+      zarinpal
+        .PaymentVerification({
+          Amount: payment.amount,
+          Authority: data.Authority,
+        })
+        .then(async (response) => {
+          if (response.status === -21) {
+            try {
+              res.status(500).send({
+                message: `Payment failed with error ${response.status}`,
+              });
+            } catch (error) {
+              res.status(500).send({ message: error.message });
+            }
+          } else {
+            await Payment.findByIdAndUpdate(String(payment._id), {
+              $set: { verified: true, refid: response.RefID },
+            });
+
+            await User.findByIdAndUpdate(String(payment.user), {
+              $inc: { credits: payment.amount },
+            });
+
+            res.redirect("https://dashboard.tfasoft.com/panel");
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({ message: error.message });
+        });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  } else {
+    res.status(500).send({ message: "Payment failed" });
+  }
+};
